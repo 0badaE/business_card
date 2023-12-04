d05610c3f90adb781f61414ef959e5dc94b2714c
Obi.É <105763414+0badaE@users.noreply.github.com>
2023-12-03 23:28:00 -0500

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,10 +4,10 @@ function Footer(){
     return(
        <footer> 
         <div className="socials">
-           <button><i className="fa-brands fa-x-twitter fa-xl"style={{color: "##161619",}} ></i></button>
-           <button> <i className="fa-brands fa-facebook fa-xl" style={{color: "##161619",}} ></i></button>
-           <button> <i className="fa-brands fa-instagram fa-xl" style={{color: "##161619",}} ></i></button>
-           <button> <i className="fa-brands fa-github fa-xl" style={{color: "##161619",}} ></i></button>
+           <button><a href ="https://twitter.com/ObadaErfanDev"> <i className="fa-brands fa-x-twitter fa-xl"style={{color: "##161619",}} ></i></a></button>
+           <button><a href ="https://www.facebook.com/Obada.erfan"> <i className="fa-brands fa-facebook fa-xl" style={{color: "##161619",}} ></i></a></button>
+           <button> <a href ="https://www.instagram.com/javascript.js/"><i className="fa-brands fa-instagram fa-xl" style={{color: "##161619",}} ></i></a></button>
+           <button> <a href ="https://github.com/0badaE"><i className="fa-brands fa-github fa-xl" style={{color: "##161619",}} ></i></a></button>
          </div>
         </footer> 
     )
