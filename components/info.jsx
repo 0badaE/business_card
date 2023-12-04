58eb756686b47a99829e93df1554e75a607960d1
Obi.É <105763414+0badaE@users.noreply.github.com>
2023-12-03 23:36:54 -0500

    Update info.jsx

--- components/info.jsx
+++ components/info.jsx
@@ -10,8 +10,8 @@ function Info(){
                  <a href = "#"><p>ObadaErfanWebDev.com </p></a>
             </section>
             <section id="Info2">
-                 <button><i className="fa-regular fa-envelope"></i>Email </button> 
-                 <button><i className="fa-brands fa-linkedin"> </i>LinkedIn</button>
+                 <button><a href="mailto:ObadaErfanDev@Gmail.com"><i class="fa-regular fa-envelope" aria-hidden="true"></i>Email </a></button>
+                 <button><a href="https://www.linkedin.com/in/obada-erfan-425031139/"><i class="fa-regular fa-envelope" aria-hidden="true"></i>Email </a></button>
             </section>
        </>
     )
