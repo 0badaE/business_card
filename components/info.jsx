29fd85f34f687d7155d98a8da462e08f28190e02
Obi.É <105763414+0badaE@users.noreply.github.com>
2023-12-05 01:34:17 -0500

    Update info.jsx

--- components/info.jsx
+++ components/info.jsx
@@ -3,7 +3,7 @@ import React from "react"
 function Info(){
     return(
        <>
-            <img src="https://lh3.googleusercontent.com/pw/ADCreHeGY269KZ1L9RUciiLQIYaLRbm-OAA1E4fs7r_cptFFsg8NRuoaIPMR9cpxrmxOBlR-6aY4t0mYqhZ9yccsGtEBAcwpVJum_6vMetwAQ70ESLpmwQ7kqQEjslbRR0Vs7Ae7Jhy5_ftBpgBRu40MiV4oKSkA_jJqUgR2bwHUZp6LRAg_8Bel0yJDpoonBrAnyiy0wiCI8bup2DVAydRYOAdjxc6DgPdFjO2oriVOnaw8CWDL4kkM-xiB04gQIx_gxaY8QJ3EMaRYpFn93ZIBBZc5GnwnJLtk0rjPWbNf1f_f_O8TzxQtyCZ_M7FhNhqP2ycvtrOIPpodOZKCfSHcLc8WJF7dhr5mU6osiclMTk8WCXVXS13Eq97TVNBzZLwyC-_4h3-_LtdnwCpq26jhDtBs1NzupbGUD6TiTnw0PpjD_e0nWaVBh3jUZyzdKjHXWJTrRddcU1FTKL8nXhQhP76_2LrJGiFc3p5gNe5LOEEza5tKxf4p0bi0Il7vbE7hPDxU76Ee4QYDhDEfWZ7z8MT3o3T3tqJd-8Ah-ng2bF1WfI5VHMaCgLVlNma00qzo-ts33ROp3hBdb1RCkvFBGM4jRdEWZWGiXLZQiqKUGugpGhFbvMJ515DRwuXtHxgJcUerfJ6GBNG4iglkTfMnNu0h-fAywq3bFXMs5sOM_b-PvCHAuLwtJpRbjJWDwQLUwwYk6jwj0YBcKjwd4GdDG9KCsIvBal73syuFf2bKYnkYH0fn6dT9IQZt1YpBTm5tZYJX49X3w2rsj68VAUSnR8HWrldEBB7tc0QR_coAdrDKIu6TNhOzyNF-MhI1DsB_2LlK7KdZGXMesPZDTJg9gzcZYm5dPzdPrGxpgAMSZQMeTD8bsgfXH39IHEaIjAz1PbLezMvgU5EoforjPx5swIbySD2ecoPS2tZRyz4ORh8phk68YGTe5IK3GcLfrnf1IChsasnMKgZAL4kaH0fjY5Y5iDMRL49Tr6Os1cYla6inRy4=w735-h911-s-no-gm?authuser=0" width = "317px"/>
+            <img src="https://ibb.co/kSphnt3"/>
             <section id="Info1">
                  <h1> Obada Erfan </h1>
                  <h3> Frontend Developer </h3>
